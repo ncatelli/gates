83f8b8e39e3971f376b8dd2c9fcd25cb9d9b14a8
Nate Catelli <ncatelli@packetfire.org>
2022-06-18 03:52:32 +0000

    Stub out the body of generic gate

--- pkg/gate/gate.go
+++ pkg/gate/gate.go
@@ -1,10 +1,115 @@
 package gate
 
+import (
+	"fmt"
+	"unicode"
+)
+
 type IO bool
 
 type Gate interface {
-	InputCnt() uint
-	InputPaths() []string
-	CurrentTick() uint
-	Tick(tick uint, inputs []IO)
+	Inputs() uint
+	Compute(tick uint, input []IO) (IO, error)
+}
+
+type PendingInput struct {
+	received bool
+	state    IO
+}
+
+type TickState struct {
+	inputs []PendingInput
+}
+
+func NewTickState(expectedInputs uint) *TickState {
+	inputs := make([]PendingInput, expectedInputs)
+
+	for i := 0; i < int(expectedInputs); i++ {
+		inputs[i] = PendingInput{
+			received: false,
+			state:    false,
+		}
+	}
+
+	return &TickState{
+		inputs: inputs,
+	}
+}
+
+type GenericGate struct {
+	ticks          map[uint]*TickState
+	expectedInputs uint
+	gate           Gate
+}
+
+func NewGenericGate(g Gate) *GenericGate {
+	return &GenericGate{
+		ticks:          make(map[uint]*TickState),
+		expectedInputs: g.Inputs(),
+		gate:           g,
+	}
+}
+
+func (hg *GenericGate) Inputs() uint {
+	return hg.expectedInputs
+}
+
+func (hg *GenericGate) Compute(tick uint, inputs []IO) (IO, error) {
+	if len(inputs) != int(hg.Inputs()) {
+		return false, fmt.Errorf("input does not match expected length of %d", hg.Inputs())
+	}
+
+	return hg.gate.Compute(tick, inputs)
+
+}
+
+func (hg *GenericGate) ReceiveInput(tick uint, input rune, state IO) (*TickState, error) {
+	// calculate the offset of the input from the rune (path)
+	inputOffset, err := runeToNormalizedOffset(input)
+	if err != nil {
+		return nil, err
+	}
+
+	// verify that the input is within range
+	if (inputOffset + 1) > hg.expectedInputs {
+		return nil, fmt.Errorf("offset %v(%d) greater than max %d", input, inputOffset, hg.expectedInputs)
+	}
+
+	ts, prs := hg.ticks[tick]
+	if prs {
+		ts.inputs[inputOffset] = PendingInput{
+			received: true,
+			state:    state,
+		}
+	} else {
+		ts := NewTickState(hg.expectedInputs)
+		hg.ticks[tick] = ts
+	}
+
+	// check if all inputs have been received, if not return early.
+	for _, input := range ts.inputs {
+		if !input.received {
+			return nil, nil
+		}
+	}
+
+	// lastly if all inputs are ready, return the tick state, representing
+	// completed tick.
+	return ts, nil
+}
+
+func runeToNormalizedOffset(r rune) (uint, error) {
+	min := uint('a')
+	max := uint('z')
+	normalized := unicode.ToLower(r)
+	runeAsInt := uint(normalized)
+
+	if runeAsInt <= max || runeAsInt >= min {
+		offset := runeAsInt - min
+
+		return offset, nil
+	}
+
+	return 0, fmt.Errorf("value out of range: must be between a-z, got %v", r)
+
 }
--- pkg/gate/not.go
+++ pkg/gate/not.go
@@ -1,20 +1,22 @@
 package gate
 
-type Not struct {
-	tick uint
-}
+import (
+	"fmt"
+)
+
+type Not struct{}
 
 func (not *Not) Inputs() uint {
 	return 1
 }
 
-func (not *Not) InputPaths() []string {
-	return []string{"a"}
-}
+func (not *Not) Compute(tick uint, inputs []IO) (IO, error) {
+	if len(inputs) != int(not.Inputs()) {
+		return false, fmt.Errorf("input does not match expected length of %d", not.Inputs())
+	}
 
-func (not *Not) CurrentTick() uint {
-	return not.tick
-}
+	input := inputs[0]
+	output := !input
 
-func (not *Not) Tick(tick uint, inputs []IO) {
+	return output, nil
 }
