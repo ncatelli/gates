9b66d8474e58caa30dbb05d3fda7978744b0fe0d
Nate Catelli <ncatelli@packetfire.org>
2022-06-19 02:47:41 +0000

    additional router tests

--- pkg/router/router_test.go
+++ pkg/router/router_test.go
@@ -1,24 +1,35 @@
 package router
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/gorilla/mux"
 	"github.com/ncatelli/gates/pkg/models"
 )
 
-type mockPathGenerator struct{}
-
-func (mpg *mockPathGenerator) RegisterPath(mux *mux.Router, msg chan<- models.MessageInput) error {
-	return nil
+type mockPathGenerator struct {
+	err error
 }
 
-func TestRouterShouldInstantiateMuxWhenPassedValidPathGenerator(t *testing.T) {
+func (mpg *mockPathGenerator) RegisterPath(mux *mux.Router, msg chan<- models.MessageInput) error {
+	return mpg.err
+}
+
+func TestNewShouldInstantiateRouterWhenPassedValidPathGenerator(t *testing.T) {
 	ibc := make(chan models.MessageInput)
-	mux, err := New(&mockPathGenerator{}, ibc)
+	mux, err := New(&mockPathGenerator{err: nil}, ibc)
 	if mux == nil {
 		t.Fatal("mux should be non-nil on valid input.")
 	} else if err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestNewShouldErrorWhenPassedAnInvalidPG(t *testing.T) {
+	ibc := make(chan models.MessageInput)
+	mux, err := New(&mockPathGenerator{err: errors.New("mock error")}, ibc)
+	if mux != nil && err == nil {
+		t.Fatal("router should fail to instantiate")
 	}
 }
